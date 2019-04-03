eba3ca0a125ae2c2a1101680dfe0a59c1d135513
ut_mat <ut_mat@A33COMP72>
2019-04-03 18:32:25 +0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -2,80 +2,79 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 
-class StarService extends Component{
-  constructor(){
+class StarService extends Component {
+  constructor() {
     super();
-    this.state={
-      starData:null,
+    this.state = {
+      starData: null,
     }
   }
 
-  componentDidMount(){
-    const id=this.props.id;
-    const apiURL="https://swapi.co/api/planets/?page="+id;
+  componentDidMount() {
+    const id = this.props.id;
+    const apiURL = "https://swapi.co/api/planets/?page=" + id;
     fetch(apiURL)
-      .then(res=>res.json())
-      .then(json=>{this.setState({starData:json})})
+      .then(res => res.json())
+      .then(json => { this.setState({ starData: json }) })
   }
 
-  render(){
-    const starData=this.state.starData;
-    if(!starData)return(<div>Waiting for a Force</div>);
-    const planet=starData.result;
-    console.log(planet);
-    return(
-      planet.map((planet,index)=>(
-      <div>
-        <h1>{planet.planet} {starData.planet}</h1>
-        <p>Name: {planet.name}</p>
-        <p>Rotation period: {planet.rotation_period}</p>
-        <p>Orbital period: {planet.orbital_period}</p>
-        <p>Diameter: {planet.diameter}</p>
-        <p>Climate: {planet.climat}</p>
-        <p>Gravity: {planet.gravity}</p>
-        <p>Terrain: {planet.terrain}</p>
-        <p>Surface water: {planet.surface_water}</p>
-        <p>Population: {planet.population}</p>
-      </div>
+  render() {
+    const starData = this.state.starData;
+    if (!starData) return (<div>Waiting for a Force</div>);
+    const planets = starData.results;
+    console.log(planets);
+    return (
+      planets.map((planet, index) => (
+        <div>
+          <p>Name: {planet.name}</p>
+          <p>Rotation period: {planet.rotation_period}</p>
+          <p>Orbital period: {planet.orbital_period}</p>
+          <p>Diameter: {planet.diameter}</p>
+          <p>Climate: {planet.climat}</p>
+          <p>Gravity: {planet.gravity}</p>
+          <p>Terrain: {planet.terrain}</p>
+          <p>Surface water: {planet.surface_water}</p>
+          <p>Population: {planet.population}</p>
+        </div>
       ))
     );
   }
 }
 
-const PAGES=[
-  {name:"1",id:"1"},
-  {name:"2",id:"2"},
-  {name:"3",id:"3"},
-  {name:"4",id:"4"},
-  {name:"5",id:"5"},
-  {name:"6",id:"6"},
-  {name:"7",id:"7"},
+const PAGES = [
+  { name: "1", id: "1" },
+  { name: "2", id: "2" },
+  { name: "3", id: "3" },
+  { name: "4", id: "4" },
+  { name: "5", id: "5" },
+  { name: "6", id: "6" },
+  { name: "7", id: "7" },
 ];
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      activePage:0,
+    this.state = {
+      activePage: 0,
     }
   }
 
   render() {
-    const activePage=this.state.activePage;
+    const activePage = this.state.activePage;
     return (
       <div className="App">
         {
-          PAGES.map((page,index)=>(
+          PAGES.map((page, index) => (
             <button key={index}
-            className={"button is-primary"}
-                    onClick={()=>{
-                      // console.log(`Button ${index} clicked!`)
-                      this.setState({activePage:index})
-                    }}>
+              className={"button is-primary"}
+              onClick={() => {
+                // console.log(`Button ${index} clicked!`)
+                this.setState({ activePage: index })
+              }}>
               {page.name}
             </button>
           ))}
-        <StarService id={PAGES[activePage].id} key={activePage}/>
+        <StarService id={PAGES[activePage].id} key={activePage} />
       </div>
     );
   }
